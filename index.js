cb8b2b7b52585ff37ea1a15a842f073bc1cd03bf
Arnaud293 <arnaudlt293@gmail.com>
2022-09-22 15:59:38 +0200

    Add left property for anim, put the logic in a function

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const bubble = document.createElement('span');
-const size = Math.random() * 200 + 100 + 'px';
+
+const bubbleMaker = () => {
+
+    const bubble = document.createElement('span');
+    const size = Math.random() * 200 + 100 + 'px';
 
 
-bubble.classList.add('bubble');
-document.body.appendChild(bubble);
+    bubble.classList.add('bubble');
+    document.body.appendChild(bubble);
 
-bubble.style.height = size;
-bubble.style.width = size;
-bubble.style.top = Math.random() * 100 + 50 + "%";
-bubble.style.left = Math.random() * 100 + "%";
+    bubble.style.height = size;
+    bubble.style.width = size;
+    bubble.style.top = Math.random() * 100 + 50 + "%";
+    bubble.style.left = Math.random() * 100 + "%";
 
+    bubble.style.setProperty('--left', Math.random()* 100 + '%');
+
+}
